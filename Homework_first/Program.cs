18c20c9a295d972f2c3bd8ffafa24a5f05711ea9
Lex <Lexperunovsky@gmail.com>
2022-12-15 23:24:44 +0300

    Make First Ex

--- Homework_first/Program.cs
+++ Homework_first/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задание 1.Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое (целое) число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе (целое) число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max;
+int min;
+if (number1 > number2) 
+{
+max = number1;
+min = number2;
+}
+else 
+{
+max = number2;
+min = number1;
+}
+Console.Write($"Максимальное число = {max}, а минимальное = {min}");
